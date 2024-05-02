7b1d51ff1ae9763fc401b4bc47e7edfa8a5b23ce
NIASSE Fama <fama.niasse@renault.com>
2024-05-03 01:19:06 +0200

    config middleware & checkToken

--- src/routes/PlanteRoute.ts
+++ src/routes/PlanteRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import PlanteController from "../controllers/PlanteController";
+import checkToken from "../middleware/CheckToken";
 
 
 const planteRouter = Router();
 const planteController = new PlanteController();
 
-planteRouter.get("/", (req, res) => {
+planteRouter.get("/", checkToken, (req, res) => {
     console.log("PlanteRouter");
     planteController.getAll(req,res);
 })
